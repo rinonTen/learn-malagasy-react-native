f8cc483a46261fb23e39fa4541aa6f48e7e98949
Rinon <rinon.ten@onja.org>
2021-04-23 14:51:06 +0300

    Adding next button component with its story

---
+++ src/components/NextButton/NextButton.js
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import { SafeAreaView, Button, StyleSheet, TouchableOpacity, Text} from 'react-native';
+
+const styles = StyleSheet.create({
+  button: { 
+    maxWidth: 90,
+    height: 40,
+    paddingTop: 8,
+    borderColor: "#06B6D4",
+    borderWidth: 1,
+    borderRadius: 30,
+  },
+  enabledButton: { 
+    backgroundColor: "#06B6D4",
+  },
+  buttonText: {
+    fontSize: 16,
+    lineHeight: 19, 
+    color: "#ffffff",
+    textAlign: "center"
+  }, 
+  disabledButton: {
+    backgroundColor: "#ffffff",
+  },
+  disabledButtonText: {
+     color: "#06B6D4",
+  },
+  longTextButton: { 
+    maxWidth: 100,
+    paddingLeft: 10,
+    paddingRight: 10,
+  }
+})
+ 
+export default function NextButton({
+  title,  
+  isDisabled, 
+  onPress = () => null,
+}) {
+
+  const buttonStyles = !isDisabled ? [styles.button, styles.enabledButton]: [styles.button, styles.disabledButton];
+  const textStyles = !isDisabled ? [styles.buttonText, styles.enabledButtonText]: [styles.buttonText, styles.disabledButtonText];
+  
+  return (
+    <SafeAreaView>
+      <TouchableOpacity
+        style={buttonStyles}
+        disabled={isDisabled}
+        onPress={onPress} 
+      >
+        <Text style={textStyles}>{title}</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+}
