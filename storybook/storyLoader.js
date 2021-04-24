11e655ec7e68aa4cea38b9dd93a5327c44460495
Rinon <rinon.ten@onja.org>
2021-04-24 14:58:44 +0300

    Adding tool button component with its storybooks

--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -6,6 +6,7 @@
 function loadStories() {
   require('../src/components/Example/Example.stories');
   require('../src/components/myComponent/myComponent.stories');
+  require('../src/components/ToolButton/ToolButton.stories');
   require('./stories/Button/Button.stories');
   require('./stories/Welcome/Welcome.stories');
 }
@@ -13,6 +14,7 @@ function loadStories() {
 const stories = [
   '../src/components/Example/Example.stories',
   '../src/components/myComponent/myComponent.stories',
+  '../src/components/ToolButton/ToolButton.stories',
   './stories/Button/Button.stories',
   './stories/Welcome/Welcome.stories',
 ];
