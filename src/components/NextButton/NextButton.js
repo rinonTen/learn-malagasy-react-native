f7a95510396542d1d27504ffa6a375e0416e99a4
Rinon <rinon.ten@onja.org>
2021-04-24 15:23:02 +0300

    Resolving conflict after merging

--- src/components/NextButton/NextButton.js
+++ src/components/NextButton/NextButton.js
@@ -1,13 +1,5 @@
 import * as React from 'react';
-<<<<<<< HEAD
 import { StyleSheet, SafeAreaView, TouchableOpacity, Text} from 'react-native';
-=======
-<<<<<<< HEAD
-import { StyleSheet, SafeAreaView, TouchableOpacity, Text} from 'react-native';
-=======
-import { SafeAreaView, Button, StyleSheet, TouchableOpacity, Text} from 'react-native';
->>>>>>> Adding next button component with its story
->>>>>>> main
 
 const styles = StyleSheet.create({
   button: { 
@@ -32,20 +24,7 @@ const styles = StyleSheet.create({
   },
   disabledButtonText: {
      color: "#06B6D4",
-<<<<<<< HEAD
   }, 
-=======
-<<<<<<< HEAD
-  }, 
-=======
-  },
-  longTextButton: { 
-    maxWidth: 100,
-    paddingLeft: 10,
-    paddingRight: 10,
-  }
->>>>>>> Adding next button component with its story
->>>>>>> main
 })
  
 export default function NextButton({
@@ -53,15 +32,7 @@ export default function NextButton({
   isDisabled, 
   onPress = () => null,
 }) {
-<<<<<<< HEAD
 // Conditions for the styling the button when it is disabled or not
-=======
-<<<<<<< HEAD
-// Conditions for the styling the button when it is disabled or not
-=======
-
->>>>>>> Adding next button component with its story
->>>>>>> main
   const buttonStyles = !isDisabled ? [styles.button, styles.enabledButton]: [styles.button, styles.disabledButton];
   const textStyles = !isDisabled ? [styles.buttonText, styles.enabledButtonText]: [styles.buttonText, styles.disabledButtonText];
   
