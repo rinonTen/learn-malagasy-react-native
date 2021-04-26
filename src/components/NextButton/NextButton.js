3fea638c2154b767a16408254fae1294f401fda3
Rinon <rinon.ten@onja.org>
2021-04-26 10:38:35 +0300

    Building a component with a storybook for the section heading

--- src/components/NextButton/NextButton.js
+++ src/components/NextButton/NextButton.js
@@ -1,65 +1,47 @@
 import * as React from 'react';
-<<<<<<< HEAD
-import { StyleSheet, SafeAreaView, TouchableOpacity, Text} from 'react-native';
-=======
-import { SafeAreaView, Button, StyleSheet, TouchableOpacity, Text} from 'react-native';
->>>>>>> Adding next button component with its story
+import {StyleSheet, SafeAreaView, TouchableOpacity, Text} from 'react-native';
 
 const styles = StyleSheet.create({
-  button: { 
+  button: {
     maxWidth: 90,
     height: 40,
     paddingTop: 8,
-    borderColor: "#06B6D4",
+    borderColor: '#06B6D4',
     borderWidth: 1,
     borderRadius: 30,
   },
-  enabledButton: { 
-    backgroundColor: "#06B6D4",
+  enabledButton: {
+    backgroundColor: '#06B6D4',
   },
   buttonText: {
     fontSize: 16,
-    lineHeight: 19, 
-    color: "#ffffff",
-    textAlign: "center"
-  }, 
+    lineHeight: 19,
+    color: '#ffffff',
+    textAlign: 'center',
+  },
   disabledButton: {
-    backgroundColor: "#ffffff",
+    backgroundColor: '#ffffff',
   },
   disabledButtonText: {
-     color: "#06B6D4",
-<<<<<<< HEAD
-  }, 
-=======
+    color: '#06B6D4',
   },
-  longTextButton: { 
-    maxWidth: 100,
-    paddingLeft: 10,
-    paddingRight: 10,
-  }
->>>>>>> Adding next button component with its story
-})
- 
-export default function NextButton({
-  title,  
-  isDisabled, 
-  onPress = () => null,
-}) {
-<<<<<<< HEAD
-// Conditions for the styling the button when it is disabled or not
-=======
+});
 
->>>>>>> Adding next button component with its story
-  const buttonStyles = !isDisabled ? [styles.button, styles.enabledButton]: [styles.button, styles.disabledButton];
-  const textStyles = !isDisabled ? [styles.buttonText, styles.enabledButtonText]: [styles.buttonText, styles.disabledButtonText];
-  
+export default function NextButton({title, isDisabled, onPress = () => null}) {
+  // Conditions for the styling the button when it is disabled or not
+  const buttonStyles = !isDisabled
+    ? [styles.button, styles.enabledButton]
+    : [styles.button, styles.disabledButton];
+  const textStyles = !isDisabled
+    ? [styles.buttonText, styles.enabledButtonText]
+    : [styles.buttonText, styles.disabledButtonText];
+
   return (
     <SafeAreaView>
       <TouchableOpacity
         style={buttonStyles}
         disabled={isDisabled}
-        onPress={onPress} 
-      >
+        onPress={onPress}>
         <Text style={textStyles}>{title}</Text>
       </TouchableOpacity>
     </SafeAreaView>
