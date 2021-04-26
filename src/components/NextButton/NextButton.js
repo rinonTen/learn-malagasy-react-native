8f3264f86cf38c9719dcf2569a18522bd2d05cd7
Rinon <rinon.ten@onja.org>
2021-04-26 10:38:35 +0300

    Building a component with a storybook for the section heading

--- src/components/NextButton/NextButton.js
+++ src/components/NextButton/NextButton.js
@@ -1,28 +1,33 @@
 import * as React from 'react';
+<<<<<<< HEAD
 import { StyleSheet, SafeAreaView, TouchableOpacity, Text} from 'react-native';
+=======
+import {StyleSheet, SafeAreaView, TouchableOpacity, Text} from 'react-native';
+>>>>>>> Building a component with a storybook for the section heading
 
 const styles = StyleSheet.create({
-  button: { 
+  button: {
     maxWidth: 90,
     height: 40,
     paddingTop: 8,
-    borderColor: "#06B6D4",
+    borderColor: '#06B6D4',
     borderWidth: 1,
     borderRadius: 30,
   },
-  enabledButton: { 
-    backgroundColor: "#06B6D4",
+  enabledButton: {
+    backgroundColor: '#06B6D4',
   },
   buttonText: {
     fontSize: 16,
-    lineHeight: 19, 
-    color: "#ffffff",
-    textAlign: "center"
-  }, 
+    lineHeight: 19,
+    color: '#ffffff',
+    textAlign: 'center',
+  },
   disabledButton: {
-    backgroundColor: "#ffffff",
+    backgroundColor: '#ffffff',
   },
   disabledButtonText: {
+<<<<<<< HEAD
      color: "#06B6D4",
   }, 
 })
@@ -36,13 +41,27 @@ export default function NextButton({
   const buttonStyles = !isDisabled ? [styles.button, styles.enabledButton]: [styles.button, styles.disabledButton];
   const textStyles = !isDisabled ? [styles.buttonText, styles.enabledButtonText]: [styles.buttonText, styles.disabledButtonText];
   
+=======
+    color: '#06B6D4',
+  },
+});
+
+export default function NextButton({title, isDisabled, onPress = () => null}) {
+  // Conditions for the styling the button when it is disabled or not
+  const buttonStyles = !isDisabled
+    ? [styles.button, styles.enabledButton]
+    : [styles.button, styles.disabledButton];
+  const textStyles = !isDisabled
+    ? [styles.buttonText, styles.enabledButtonText]
+    : [styles.buttonText, styles.disabledButtonText];
+
+>>>>>>> Building a component with a storybook for the section heading
   return (
     <SafeAreaView>
       <TouchableOpacity
         style={buttonStyles}
         disabled={isDisabled}
-        onPress={onPress} 
-      >
+        onPress={onPress}>
         <Text style={textStyles}>{title}</Text>
       </TouchableOpacity>
     </SafeAreaView>
