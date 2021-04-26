3f20e90d10362bdac3b345ac1c2543822670e675
Rinon <rinon.ten@onja.org>
2021-04-26 11:46:44 +0300

    Resolving conflicts when rebasing

--- src/components/NextButton/NextButton.js
+++ src/components/NextButton/NextButton.js
@@ -1,9 +1,5 @@
 import * as React from 'react';
-<<<<<<< HEAD
-import { StyleSheet, SafeAreaView, TouchableOpacity, Text} from 'react-native';
-=======
 import {StyleSheet, SafeAreaView, TouchableOpacity, Text} from 'react-native';
->>>>>>> Building a component with a storybook for the section heading
 
 const styles = StyleSheet.create({
   button: {
@@ -27,21 +23,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#ffffff',
   },
   disabledButtonText: {
-<<<<<<< HEAD
-     color: "#06B6D4",
-  }, 
-})
- 
-export default function NextButton({
-  title,  
-  isDisabled, 
-  onPress = () => null,
-}) {
-// Conditions for the styling the button when it is disabled or not
-  const buttonStyles = !isDisabled ? [styles.button, styles.enabledButton]: [styles.button, styles.disabledButton];
-  const textStyles = !isDisabled ? [styles.buttonText, styles.enabledButtonText]: [styles.buttonText, styles.disabledButtonText];
-  
-=======
     color: '#06B6D4',
   },
 });
@@ -55,7 +36,6 @@ export default function NextButton({title, isDisabled, onPress = () => null}) {
     ? [styles.buttonText, styles.enabledButtonText]
     : [styles.buttonText, styles.disabledButtonText];
 
->>>>>>> Building a component with a storybook for the section heading
   return (
     <SafeAreaView>
       <TouchableOpacity
