fd57e83c70705d37bb8e14ec1d9fc4ae485b8967
Rinon <rinon.ten@onja.org>
2021-04-26 10:38:35 +0300

    Building a component with a storybook for the section heading

--- src/components/SectionHeading/SectionHeading.stories.js
+++ src/components/SectionHeading/SectionHeading.stories.js
@@ -3,8 +3,8 @@ import { View } from 'react-native';
 import { storiesOf } from '@storybook/react-native';
 import SectionHeading from './SectionHeading';
  
-storiesOf('NextButton', module)
+storiesOf('SectionHeading', module)
   .addDecorator(story => <View style={{padding: 23}}>{story()}</View>)
   .add('SectionHeadingWithShortPhrase', () => <SectionHeading text="Seen phrases:" />)
-  .add('SectionHeadingWithLongPhrase', () => (<SectionHeading text="Select a category:"/>))
+  .add('SectionHeadingWithLongPhrase', () => <SectionHeading text="Select a category from the category list:"/>)
    
