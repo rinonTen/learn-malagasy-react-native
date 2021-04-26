43e1c3edf09c55a930f3c6a05bb95a13a425d52b
Rinon <rinon.ten@onja.org>
2021-04-26 10:38:35 +0300

    Building a component with a storybook for the section heading

--- src/components/SectionHeading/SectionHeading.js
+++ src/components/SectionHeading/SectionHeading.js
@@ -1,25 +1,19 @@
 import * as React from 'react';
-import { StyleSheet, SafeAreaView, Text} from 'react-native';
+import {StyleSheet, SafeAreaView, Text} from 'react-native';
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 18,
     lineHeight: 22,
-<<<<<<< HEAD
-    fontWeight: "600",
-=======
->>>>>>> Building a component and a storybook for the section heading
-    color: "#111827"
-  }
-})
- 
-export default function NextButton({
-  text
-}) {
- 
+    fontWeight: '600',
+    color: '#111827',
+  },
+});
+
+export default function NextButton({text}) {
   return (
-    <SafeAreaView>  
-        <Text style={styles.text}>{text}</Text> 
+    <SafeAreaView>
+      <Text style={styles.text}>{text}</Text>
     </SafeAreaView>
   );
 }
--- src/components/SectionHeading/SectionHeading.stories.js
+++ src/components/SectionHeading/SectionHeading.stories.js
@@ -1,17 +1,13 @@
 import * as React from 'react';
-import { View } from 'react-native';
-import { storiesOf } from '@storybook/react-native';
+import {View} from 'react-native';
+import {storiesOf} from '@storybook/react-native';
 import SectionHeading from './SectionHeading';
- 
-<<<<<<< HEAD
+
 storiesOf('SectionHeading', module)
   .addDecorator(story => <View style={{padding: 23}}>{story()}</View>)
-  .add('SectionHeadingWithShortPhrase', () => <SectionHeading text="Seen phrases:" />)
-  .add('SectionHeadingWithLongPhrase', () => <SectionHeading text="Select a category from the category list:"/>)
-=======
-storiesOf('NextButton', module)
-  .addDecorator(story => <View style={{padding: 23}}>{story()}</View>)
-  .add('SectionHeadingWithShortPhrase', () => <SectionHeading text="Seen phrases:" />)
-  .add('SectionHeadingWithLongPhrase', () => (<SectionHeading text="Select a category:"/>))
->>>>>>> Building a component and a storybook for the section heading
-   
+  .add('SectionHeadingWithShortPhrase', () => (
+    <SectionHeading text="Seen phrases:" />
+  ))
+  .add('SectionHeadingWithLongPhrase', () => (
+    <SectionHeading text="Select a category from the category list:" />
+  ));
