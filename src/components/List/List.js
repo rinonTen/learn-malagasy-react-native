437de4c1a41c7ac1646b0be56afaa7d415d5e822
Rinon <rinon.ten@onja.org>
2021-04-28 08:38:16 +0300

    Modifying style names and adding spaces to stories

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import {
-<<<<<<< HEAD
   StyleSheet,
   View,
   SafeAreaView,
@@ -10,14 +9,14 @@ import {
 import ListItem from '../ListItem/ListItem';
 import SectionHeading from '../SectionHeading/SectionHeading';
 const styles = StyleSheet.create({
-  defaultStyles: {
+  container: {
     borderColor: '#E5E5E5',
     borderWidth: 1,
   },
 });
 
 const FlatListItemSeparator = () => {
-  return <View style={styles.defaultStyles} />;
+  return <View style={styles.container} />;
 };
 
 export default function ListItemComponent({
@@ -30,69 +29,20 @@ export default function ListItemComponent({
     <SafeAreaView>
       <SectionHeading text={heading} />
       <FlatList
-        style={[styles.defaultStyles, styles.container]}
+        style={styles.container}
         data={data}
         onPress={onPress}
         renderItem={({item}) => (
           <TouchableOpacity onPress={onPress}>
             <ListItem
               categoryName={isEnglishLanguage ? item.name.en : item.name.mg}
-              // onPress={onPress}
+              onPress={onPress}
             />
           </TouchableOpacity>
         )}
         keyExtractor={item => item.id}
         ItemSeparatorComponent={() => <FlatListItemSeparator />}
       />
-=======
-  SafeAreaView,
-  KeyboardAvoidingView,
-  StyleSheet,
-  FlatList,
-  View,
-} from 'react-native';
-import ListItem from '../ListItem/ListItem';
-import SectionHeading from '../SectionHeading/SectionHeading';
-
-const styles = StyleSheet.create({
-  commonStyles: {
-    borderColor: '#E5E5E5',
-    borderWidth: 1,
-  },
-  container: {
-    borderRadius: 3,
-  },
-});
-
-const FlatListItemSeparator = () => {
-  return <View style={styles.commonStyles} />;
-};
-
-export default function ListItemComponent({
-  onPress = () => {},
-  data,
-  isEnglishLanguage,
-  heading,
-}) {
-  return (
-    <SafeAreaView>
-      <KeyboardAvoidingView>
-        <SectionHeading text={heading} />
-        <FlatList
-          style={[styles.container, styles.commonStyles]}
-          data={data}
-          onPress={onPress}
-          renderItem={({item}) => (
-            <ListItem
-              categoryName={isEnglishLanguage ? item.name.en : item.name.mg}
-              onPress={onPress}
-            />
-          )}
-          keyExtractor={item => item.id}
-          ItemSeparatorComponent={() => <FlatListItemSeparator />}
-        />
-      </KeyboardAvoidingView>
->>>>>>> list-item
     </SafeAreaView>
   );
 }
