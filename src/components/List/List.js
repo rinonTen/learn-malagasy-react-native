5b0a2baa20692d6fccf231099c3bf5f28e68d4ce
Rinon <rinon.ten@onja.org>
2021-04-29 15:16:14 +0300

    Editing global styles, adding some styles

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -19,21 +19,12 @@ export default function ListItemComponent({
     <SafeAreaView>
       <SectionHeading text={heading} />
       <View style={{backgroundColor: '#ffffff'}}>
-<<<<<<< HEAD
         <View style={[GlobalStyles.listContainer, GlobalStyles.listBorder]}>
           {data &&
             data.map(item => {
               return (
                 <React.Fragment key={item.id}>
                   <TouchableOpacity onPress={onPress}>
-=======
-        <View style={[styles.container, styles.commonStyles]}>
-          {data &&
-            data.map(item => {
-              return (
-                <React.Fragment>
-                  <TouchableOpacity onPress={onPress} key={item.id}>
->>>>>>> Removing flatList from the list because it is not supporteb by android when nested inside scrollView
                     <ListItem
                       categoryName={
                         isEnglishLanguage ? item.name.en : item.name.mg
