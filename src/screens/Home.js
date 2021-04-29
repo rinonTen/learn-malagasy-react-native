dc6d46216284077d68ce18f04d568e0fd0cd9da1
Rinon <rinon.ten@onja.org>
2021-04-29 15:15:32 +0300

    Showing seen and learnt phrases test

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,25 +1,64 @@
 import React, {useState, useEffect} from 'react';
-import {SafeAreaView, View, ScrollView} from 'react-native';
+import {
+  StyleSheet,
+  SafeAreaView,
+  ScrollView,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
 import {useSelector, useDispatch} from 'react-redux';
 import {getCategoryList} from '../actions';
 import HomeHeader from './HomeHeader';
 import List from '../components/List/List';
+import SectionHeading from '../components/SectionHeading/SectionHeading';
+import ListItem from '../components/ListItem/ListItem';
 import GlobalStyles from '../constants/GlobalStyles';
+
+const styles = StyleSheet.create({
+  phraseListItem: {
+    backgroundColor: '#ffffff',
+    borderStartColor: '#E5E5E5',
+    borderEndColor: '#E5E5E5',
+    borderWidth: 1,
+    borderRadius: 3,
+  },
+  sectionContainer: {
+    marginTop: 15,
+  },
+});
 
 export default () => {
   const [isEnglishLanguage, setIsEnglishLanguage] = useState(true);
   const dispatch = useDispatch();
-  const state = useSelector(state => state.categoryList);
-  const {categoryList, isLoading} = state;
+  const categoryListState = useSelector(state => state.categoryList);
+  const seenPhrases = useSelector(state => state.seenPhrases);
+  const learntPhrases = useSelector(state => state.learntPhrases);
+  const {categoryList, isLoading} = categoryListState;
 
   useEffect(() => {
     dispatch(getCategoryList());
   }, []);
 
+  const PhrasesComponent = ({pharasesArr, onPress = () => {}}) => {
+    return (
+      <TouchableOpacity style={styles.phraseListItem}>
+        <ListItem
+          categoryName={
+            isEnglishLanguage
+              ? `${pharasesArr.length} seen phrases`
+              : `teny sy fehezanteny ${pharasesArr.length}`
+          }
+          onPress={onPress}
+        />
+      </TouchableOpacity>
+    );
+  };
+
   return (
     <SafeAreaView style={GlobalStyles.screen}>
       <ScrollView>
-        <View style={GlobalStyles.container}>
+        <View style={[GlobalStyles.container, {paddingBottom: 66}]}>
           <HomeHeader
             switchLanguage={() => setIsEnglishLanguage(!isEnglishLanguage)}
           />
@@ -27,8 +66,32 @@ export default () => {
             isEnglishLanguage={isEnglishLanguage}
             onPress={() => alert('Learn phrases about in this category')}
             data={categoryList && !isLoading && categoryList.categories}
-            heading="Category List"
+            heading={
+              isEnglishLanguage ? 'Category List' : 'Lisitry ny sokajy misy'
+            }
           />
+          {seenPhrases.length >= 1 && (
+            <View style={styles.sectionContainer}>
+              <SectionHeading
+                text={
+                  isEnglishLanguage ? 'Seen phrases' : 'Fehezanteny efa hita'
+                }
+              />
+              <PhrasesComponent pharasesArr={seenPhrases} />
+            </View>
+          )}
+          {learntPhrases.length >= 1 && (
+            <View style={styles.sectionContainer}>
+              <SectionHeading
+                text={
+                  isEnglishLanguage
+                    ? 'Learnt phrases'
+                    : 'Fehezanteny efa nianarana'
+                }
+              />
+              <PhrasesComponent pharasesArr={learntPhrases} />
+            </View>
+          )}
         </View>
       </ScrollView>
     </SafeAreaView>
