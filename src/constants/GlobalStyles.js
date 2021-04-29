feb8526f19e73aca4f87393a9b7aca64d61ca55b
Rinon <rinon.ten@onja.org>
2021-04-29 11:17:29 +0300

    Adding global style

---
+++ src/constants/GlobalStyles.js
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  screen: {
+    backgroundColor: '#E5E5E5',
+  },
+  container: {
+    marginLeft: 23,
+    marginRight: 23,
+  },
+});
