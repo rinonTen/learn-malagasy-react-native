effa9991cca587207a62a233010df4914c3bf517
Rinon <rinon.ten@onja.org>
2021-04-29 11:19:01 +0300

    Applying global styles to the home screen

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,49 +1,43 @@
 import * as React from 'react';
 import {TouchableOpacity} from 'react-native';
-import {
-  StyleSheet,
-  View,
-  SafeAreaView,
-  FlatList,
-  TouchableOpacity,
-} from 'react-native';
+import {SafeAreaView, View} from 'react-native';
 import ListItem from '../ListItem/ListItem';
 import SectionHeading from '../SectionHeading/SectionHeading';
-const styles = StyleSheet.create({
-  container: {
-    borderColor: '#E5E5E5',
-    borderWidth: 1,
-  },
-});
+import GlobalStyles from '../../constants/GlobalStyles';
 
 const FlatListItemSeparator = () => {
-  return <View style={styles.container} />;
+  return <View style={GlobalStyles.listBorder} />;
 };
 
 export default function ListItemComponent({
-  heading,
   onPress = () => {},
-  isEnglishLanguage,
   data,
+  isEnglishLanguage,
+  heading,
 }) {
   return (
     <SafeAreaView>
       <SectionHeading text={heading} />
-      <FlatList
-        style={styles.container}
-        data={data}
-        onPress={onPress}
-        renderItem={({item}) => (
-          <TouchableOpacity onPress={onPress}>
-            <ListItem
-              categoryName={isEnglishLanguage ? item.name.en : item.name.mg}
-              onPress={onPress}
-            />
-          </TouchableOpacity>
-        )}
-        keyExtractor={item => item.id}
-        ItemSeparatorComponent={() => <FlatListItemSeparator />}
-      />
+      <View style={{backgroundColor: '#ffffff'}}>
+        <View style={[GlobalStyles.listContainer, GlobalStyles.listBorder]}>
+          {data &&
+            data.map(item => {
+              return (
+                <React.Fragment key={item.id}>
+                  <TouchableOpacity onPress={onPress}>
+                    <ListItem
+                      categoryName={
+                        isEnglishLanguage ? item.name.en : item.name.mg
+                      }
+                      onPress={onPress}
+                    />
+                  </TouchableOpacity>
+                  <FlatListItemSeparator />
+                </React.Fragment>
+              );
+            })}
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
