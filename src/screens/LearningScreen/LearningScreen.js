7736d34a07f28fa709033bec5895373396e3a299
Rinon <rinon.ten@onja.org>
2021-04-30 09:29:39 +0300

    Creating a folder for learning screen

---
+++ src/screens/LearningScreen/LearningScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
+
+export default () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>I am a screen!</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
