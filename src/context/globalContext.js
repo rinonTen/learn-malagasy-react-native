60b9a2ce5ddacbf4273b69707bdf375d6ca12855
Rinon <rinon.ten@onja.org>
2021-05-03 11:43:34 +0300

    Creating a global context file to store some states used to display items in the learning screen

--- src/context/globalContext.js
+++ src/context/globalContext.js
@@ -16,7 +16,7 @@ export const useContext = () => {
   const seenPhrases = useSelector(state => state.seenPhrases);
   const learntPhrases = useSelector(state => state.learntPhrases);
   const phrasesState = useSelector(state => state.phrases);
-
+  const categoryListId = useSelector(state => state.categoryListId);
   const {categoryList, isLoading} = categoryListState;
   const {phrases, learningPhrases} = phrasesState;
 
@@ -29,23 +29,74 @@ export const useContext = () => {
     return arr && arr[Math.floor(Math.random() * arr.length)];
   }
 
+  // Function picking random values from array
+  const getRandomIdFromArray = phrasesArray => {
+    let arrayOfAlreadyUsedIds = [];
+    // If arrayOfAlreadyUsedIds is empty then fill it will indexes equal to the size of phrasesArray
+    if (arrayOfAlreadyUsedIds.length === 0) {
+      for (let i = 0; i < phrasesArray.length; i++)
+        arrayOfAlreadyUsedIds.push(i);
+    }
+    let randomValueIndex = Math.floor(
+      Math.random() * arrayOfAlreadyUsedIds.length,
+    );
+    let indexOfItemInTheArray = arrayOfAlreadyUsedIds[randomValueIndex];
+    // remove this index from arrayOfAlreadyUsedIds array because we are accessing it now.
+    arrayOfAlreadyUsedIds.splice(randomValueIndex, 1);
+    // Get the value
+    return phrasesArray[indexOfItemInTheArray];
+  };
+
   const getPhraseForCategory = () => {
     const category =
       categoryList &&
       !isLoading &&
-      categoryList.categories.find(
-        category => category.id === categoryToDisplayId,
-      );
-    console.log(categoryToDisplayId);
+      categoryList.categories.find(category => category.id === categoryListId);
     const randomIdOne = getRandomItem(category?.phrasesIds);
     const randomIdTwo = getRandomItem(category?.phrasesIds);
     const randomIdThree = getRandomItem(category?.phrasesIds);
     const randomIdFour = getRandomItem(category?.phrasesIds);
     setCategoryName(isEnglishLanguage ? category?.name.en : category?.name.ma);
-    setPhraseIdToDisplay1(randomIdOne);
-    setPhraseIdToDisplay2(randomIdTwo);
-    setPhraseIdToDisplay3(randomIdThree);
-    setPhraseIdToDisplay4(randomIdFour);
+    // Conditions to make phrases displayed in the learning screen to be different
+    if (
+      randomIdOne !== randomIdTwo &&
+      randomIdOne !== randomIdThree &&
+      randomIdOne !== randomIdFour
+    ) {
+      setPhraseIdToDisplay1(randomIdOne);
+    } else {
+      setPhraseIdToDisplay1(getRandomItem(category?.phrasesIds));
+    }
+
+    if (
+      randomIdTwo !== randomIdOne &&
+      randomIdTwo !== randomIdThree &&
+      randomIdTwo !== randomIdFour
+    ) {
+      setPhraseIdToDisplay2(randomIdTwo);
+    } else {
+      setPhraseIdToDisplay2(getRandomItem(category?.phrasesIds));
+    }
+
+    if (
+      randomIdThree !== randomIdOne &&
+      randomIdThree !== randomIdTwo &&
+      randomIdThree !== randomIdFour
+    ) {
+      setPhraseIdToDisplay3(randomIdThree);
+    } else {
+      setPhraseIdToDisplay3(getRandomItem(category?.phrasesIds));
+    }
+
+    if (
+      randomIdFour !== randomIdOne &&
+      randomIdFour !== randomIdTwo &&
+      randomIdFour !== randomIdThree
+    ) {
+      setPhraseIdToDisplay4(randomIdFour);
+    } else {
+      setPhraseIdToDisplay4(getRandomItem(category?.phrasesIds));
+    }
   };
 
   useEffect(() => {
