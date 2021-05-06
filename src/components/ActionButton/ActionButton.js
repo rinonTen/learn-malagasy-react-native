94e52ffa2247398dd24ef4f7e8d07dc3e1f87b50
Rinon <rinon.ten@onja.org>
2021-05-06 11:32:17 +0300

    Adding conditions to change icons and text according to the picked answer

--- src/components/ActionButton/ActionButton.js
+++ src/components/ActionButton/ActionButton.js
@@ -27,20 +27,26 @@ export default function ActionButton({
   title,
   onPress = () => {},
   isCorrect,
+  isIncorrect,
   isDisabled,
 }) {
   let buttonIcon;
   let textStyles;
+  let buttonText;
 
   if (isCorrect) {
     buttonIcon = <CorrectAnswerIcon />;
     textStyles = [styles.greenText, styles.text];
-  } else if (!isCorrect && isDisabled) {
+    buttonText = 'Correct';
+  } else if (isIncorrect) {
     buttonIcon = <WrongAnswerIcon />;
     textStyles = [styles.redText, styles.text];
+    buttonText = 'Wrong';
   } else {
     buttonIcon = <RightArrowIcon />;
     textStyles = [styles.blueText, styles.text];
+    // Pass the props
+    buttonText = title;
   }
 
   return (
@@ -48,9 +54,8 @@ export default function ActionButton({
       <TouchableOpacity
         onPress={onPress}
         disabled={isDisabled}
-        style={styles.button}
-        isCorrect={isCorrect}>
-        <Text style={textStyles}>{title}</Text>
+        style={styles.button}>
+        <Text style={textStyles}>{buttonText}</Text>
         {buttonIcon}
       </TouchableOpacity>
     </SafeAreaView>
