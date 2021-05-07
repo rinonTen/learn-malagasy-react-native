8a2b9edb6e2eb29698a63990979f7e2448bc2a7b
Rinon <rinon.ten@onja.org>
2021-05-07 11:33:11 +0300

    Renaming file

--- src/listManagers/GlobalListManager.js
+++ src/listManagers/GlobalListManager.js
@@ -1,7 +1,7 @@
 import {useState, useEffect} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import {getCategoryList, setPrases} from '../actions';
-import {shufflePhrasesArr} from './Utils';
+import {shufflePhrasesArr} from './UtilsFunctions';
 
 export const globalListManager = () => {
   const [isEnglishLanguage, setIsEnglishLanguage] = useState(true);
